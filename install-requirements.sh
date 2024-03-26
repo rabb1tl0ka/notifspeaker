c2d5519eb1e2633005d59d390df90925443ddce6
Bruno @ Loka <bruno@loka.com>
2024-03-26 12:53:16 +0000

    source the install-requirements.sh file
    To ensure that the virtual environment is activated
    in your current shell session,
    you should run the shell script with the source command.
    
    When you execute the shell script, each command runs in its own subshell.
    
    using source before executing the script solves this.

--- install-requirements.sh
+++ install-requirements.sh
@@ -1,7 +1,14 @@
-#! /bin/bash
+#!/bin/bash
+
+# Check if the script is being sourced
+if [ "$0" = "$BASH_SOURCE" ]; then
+    echo "Error: This script should be sourced using 'source ./install-requirements.sh' or '.' ./install-requirements.sh'"
+    echo "Exiting..."
+    exit 1
+fi
 
 python3 -m venv .venv
 
-source .venv/bin/activate
+source .venv/bin/activate && echo "Virtual environment activated successfully"
 
 pip3 install -r requirements.txt
